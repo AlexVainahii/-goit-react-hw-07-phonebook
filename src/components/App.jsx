f0ca801f5417d0f9eab4f2a648c1b5339a733a77
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-06-18 18:53:55 +0300

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { ToastContainer } from 'react-toastify';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { P } from './ContactList/ContactList.styled';
+import { Filter } from './Filter/Filter';
+import { Section } from './Section/Section';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container, SectionContainer } from './App.styled';
+import { getContacts, getError, getIsLoading } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
+import { useEffect } from 'react';
 export const App = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <SectionContainer>
+      <ToastContainer autoClose={3000} draggable={false} />
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        {isLoading && !error && <b>Request in progress...</b>}
+        {contacts.length <= 0 ? (
+          <P>No contacts in Phonebook</P>
+        ) : (
+          <Container>
+            <Filter />
+            <ContactList />
+          </Container>
+        )}
+      </Section>
+    </SectionContainer>
   );
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,51 @@
+import { Button, Form, Input } from './ContactForm.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { addContact } from 'redux/operations';
+import { getContacts } from 'redux/selectors';
+
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const formSubmit = e => {
+    e.preventDefault();
+    const { name, number } = e.target.elements;
+    if (
+      contacts.find(
+        option => option.name.toLowerCase() === name.value.toLowerCase()
+      )
+    ) {
+      toast.warning(`${name.value} is already in contacts list`);
+      return false;
+    } else {
+      dispatch(addContact({ name: name.value, number: number.value }));
+      name.value = '';
+      number.value = '';
+      return true;
+    }
+  };
+
+  return (
+    <>
+      <Form onSubmit={formSubmit}>
+        <label htmlFor="name">Name</label>
+        <Input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <label htmlFor="number">Number</label>
+        <Input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </>
+  );
+};
